fb9ffd6133205e74b214aaf92079b56a310c18d1
shyam11-dev <morishyam11112005@gmail.com>
2026-02-18 14:44:36 +0530

    All product show

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import ProductCart from "../components/ProductCart";
 const Products = () => {
-  return <div>products</div>;
+  const { products, searchQuery } = useContext(AppContext);
+  const [filterProducts, setFilterProducts] = useState([]);
+  useEffect(() => {
+    if (searchQuery > 0) {
+      setFilterProducts(
+        products.filter((products) => {
+          products.name.toLoweCase().include(searchQuery.toLoweCase());
+        }),
+      );
+    } else {
+      setFilterProducts(products);
+    }
+  }, [searchQuery, products]);
+  return (
+    <div className="mt-16">
+      <h1 className="text-3xl lg:text-4xl font-medium">All Products</h1>
+      <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 items-center justify-center">
+        {filterProducts
+          .filter((products) => products.inStock)
+          .map((products, index) => (
+            <ProductCart key={index} products={products}></ProductCart>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
