d949fcd011df7fe25a80c7f87a948a814c9e3a2c
shyam11-dev <morishyam11112005@gmail.com>
2026-02-18 10:44:24 +0530

    card item add,total etc add

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyAddress, dummyProducts } from "../assets/assets";
-
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 export const AppContext = createContext(null);
 
 const AppContextProvider = ({ children }) => {
@@ -10,9 +10,37 @@ const AppContextProvider = ({ children }) => {
   const [isSeller, setIsSeller] = useState(null);
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
+  const [cardItems, setCardItems] = useState({});
+
   // fetch all products data
   const fetchProduct = async () => {
     setProducts(dummyProducts);
+  };
+  // add product to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cardItems);
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCardItems(cartData);
+    toast.success("add to cart");
+  };
+  //update cart item quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cardItems);
+    cartData[itemId] = quantity;
+    setCardItems(cartData);
+    toast.success("cart updated");
+  };
+  // total cart items
+  const cartCount = () => {
+    let totalCount = 0;
+    for (const items in cardItems) {
+      totalCount += cardItems[itemitemd];
+    }
+    return totalCount;
   };
   useEffect(() => {
     fetchProduct();
@@ -26,6 +54,9 @@ const AppContextProvider = ({ children }) => {
     showUserLogin,
     setShowUserLogin,
     products,
+    addToCart,
+    updateCartItem,
+    cartCount,
   };
   return <AppContext.Provider value={value}> {children}</AppContext.Provider>;
 };
