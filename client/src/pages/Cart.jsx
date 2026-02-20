f29dbdd18b448d5d2a43800fc276dae81bd3c7f3
shyam11-dev <morishyam11112005@gmail.com>
2026-02-20 22:00:52 +0530

    amount section solved

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -161,7 +161,7 @@ const Cart = () => {
 
         <div className="mb-6">
           <p className="text-sm font-medium uppercase">Delivery Address</p>
-          <div className="relative flex justify-between items-start mt-2">
+          {/* <div className="relative flex justify-between items-start mt-2">
             <p className="text-gray-500">No address found</p>
             <button
               onClick={() => setShowAddress(!showAddress)}
@@ -185,11 +185,53 @@ const Cart = () => {
                 </p>
               </div>
             )}
+          </div> */}
+          <div className="relative flex justify-between items-start mt-2">
+            <p className="text-gray-500">
+              {selectedAddress
+                ? `${selectedAddress.street}, ${selectedAddress.city}, ${selectedAddress.state}, ${selectedAddress.country}`
+                : "No Address Found"}
+            </p>
+
+            <button
+              onClick={() => setShowAddress(!showAddress)}
+              className="text-indigo-500 hover:underline cursor-pointer"
+            >
+              Change
+            </button>
+
+            {showAddress && (
+              <div className="absolute top-12 py-1 bg-white border border-gray-300 text-sm w-full">
+                {address.map((address, index) => (
+                  <p
+                    key={index}
+                    onClick={() => {
+                      setSelectedAddress(address);
+                      setShowAddress(false);
+                    }}
+                    className="text-gray-500 p-2 hover:bg-gray-100 cursor-pointer"
+                  >
+                    {address.street}, {address.city}, {address.state},{" "}
+                    {address.country}
+                  </p>
+                ))}
+
+                <p
+                  onClick={() => navigate("/add-address")}
+                  className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-gray-500/10"
+                >
+                  Add Address
+                </p>
+              </div>
+            )}
           </div>
 
           <p className="text-sm font-medium uppercase mt-6">Payment Method</p>
 
-          <select className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none">
+          <select
+            onClick={(e) => setPaymentOption(e.target.value)}
+            className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none"
+          >
             <option value="COD">Cash On Delivery</option>
             <option value="Online">Online Payment</option>
           </select>
@@ -200,7 +242,7 @@ const Cart = () => {
         <div className="text-gray-500 mt-4 space-y-2">
           <p className="flex justify-between">
             <span>Price</span>
-            <span>$20</span>
+            <span>${totalCartAmount()}</span>
           </p>
           <p className="flex justify-between">
             <span>Shipping Fee</span>
@@ -208,16 +250,16 @@ const Cart = () => {
           </p>
           <p className="flex justify-between">
             <span>Tax (2%)</span>
-            <span>$20</span>
+            <span>${(totalCartAmount() * 2) / 100}</span>
           </p>
           <p className="flex justify-between text-lg font-medium mt-3">
             <span>Total Amount:</span>
-            <span>$20</span>
+            <span>${totalCartAmount() + (totalCartAmount() * 2) / 100}</span>
           </p>
         </div>
 
         <button className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition">
-          Place Order
+          {paymentOption === "COD" ? "Place Order" : "Pay Now"}
         </button>
       </div>
     </div>
