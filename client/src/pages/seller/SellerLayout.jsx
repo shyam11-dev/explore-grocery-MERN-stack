30b10a39250ad2ec496d9e4145a36b5941d126da
shyam11-dev <morishyam11112005@gmail.com>
2026-03-03 10:27:23 +0530

    error solved

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,7 +1,74 @@
-import React from "react";
-
+import { Link, NavLink, Outlet } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import toast from "react-hot-toast";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 const SellerLayout = () => {
-  return <div>SellerLayout</div>;
-};
+  const { isSeller, setIsSeller, navigate } = useContext(AppContext);
+  const sidebarLinks = [
+    { name: "Add Product", path: "/seller", icon: assets.add_icon },
+    {
+      name: "Product List",
+      path: "/seller/product-list",
+      icon: assets.product_list_icon,
+    },
+    { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
+  ];
 
+  // const logout = async () => {
+  //   try {
+  //     const { data } = await axios.get("/api/seller/logout");
+  //     if (data.success) {
+  //       setIsSeller(false);
+  //       toast.success("Logged out successfully");
+  //       navigate("/");
+  //     }
+  //   } catch (error) {
+  //     toast.error("Failed to logout");
+  //     console.error(error);
+  //   }
+  // };
+  return (
+    <>
+      <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
+        <Link to={"/"}>
+          <h1 className="text-2xl font-semibold">Grocery Store App</h1>
+        </Link>
+        <div className="flex items-center gap-5 text-gray-500">
+          <p>Hi! Admin</p>
+          <button
+            onClick={() => {
+              setIsSeller(false);
+              navigate("/");
+            }}
+            className="border rounded-full text-sm px-4 py-1 cursor-pointer"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col ">
+          {sidebarLinks.map((item) => (
+            <NavLink
+              to={item.path}
+              key={item.name}
+              end={item.path === "/seller"}
+              className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
+                            ${
+                              isActive
+                                ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
+                                : "hover:bg-gray-100/90 border-white "
+                            }`}
+            >
+              <img src={item.icon} alt="" className="w-7 h-7" />
+              <p className="md:block hidden text-center">{item.name}</p>
+            </NavLink>
+          ))}
+        </div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 export default SellerLayout;
